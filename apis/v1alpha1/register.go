4e27a2b0bf941bcccbd331bc3e298658faf540e2
Felix Zippel <contact@zippelf.com>
2025-01-09 22:19:02 +0100

    Prepare repo

--- apis/v1alpha1/register.go
+++ apis/v1alpha1/register.go
@@ -13,7 +13,7 @@ import (
 
 // Package type metadata.
 const (
-	Group   = "template.upbound.io"
+	Group   = "hcloud.fzx.github.com"
 	Version = "v1alpha1"
 )
 
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/upbound/upjet-provider-template
+module github.com/felixz92/provider-hcloud
 
 go 1.21
 
